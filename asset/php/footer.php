c51481664111a3cd82a8c3141db54b419d257a76
Tahri Mostapha <tahri.mostapha.pro@gmail.com>
2018-08-09 15:40:35 +0200

    add GDPR Cookie

--- asset/php/footer.php
+++ asset/php/footer.php
@@ -1,3 +1,24 @@
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+    <script>
+    window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#1d8a8a"
+        },
+        "button": {
+          "background": "#62ffaa"
+        }
+      },
+      "theme": "classic",
+      "position": "bottom-left",
+      "content": {
+        "message": "Ce site utilise des cookies pour vous assurer la meilleure expérience sur notre site Web.",
+        "dismiss": "J'ai saisis!",
+        "link": "En savoir plus"
+      }
+    })});
+    </script>
     <script src="./asset/js/ajax.js"></script>
     <script src="./asset/js/script.js"></script>
   </body>
