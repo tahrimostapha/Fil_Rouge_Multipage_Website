aa83f0fbfbb89b86d9677df59e49c509cebfc467
NZOZN <2016.carlier.liam@gmail.com>
2018-08-10 16:19:26 +0200

    modifs

---
+++ asset/formulaire_de_reception.php
@@ -0,0 +1,45 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+require 'vendor/PHPMailer/PHPMailer/src/Exception.php';
+require 'vendor/PHPMailer/PHPMailer/src/PHPMailer.php';
+require 'vendor/PHPMailer/PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = '2016.carlier.liam@gmail.com';                 // SMTP username
+    $mail->Password = 'GoVegan123';                           // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('2016.carlier.liam@gmail.com', 'XVX');
+    //Set an alternative reply-to address
+    $mail->addAddress('2016.carlier.liam@gmail.com', 'Liam');     // Add a recipient
+    $Body = "<p><strong> Wesh</strong> c'est mon premiermail php <p>";
+  
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = "C'est un email test";
+    $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+
+    $mail->Body    = $Body;
+    $mail->AltBody = 'This is a plain-text message body';
+    //Attach an image file
+    // $mail->addAttachment('images/phpmailer_mini.png');
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
---
+++ asset/header.php
@@ -0,0 +1,51 @@
+<?php require '../asset/function.php' ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+<link href="./css/bulma.min.css" rel="stylesheet">
+      <link href="./css/Loveson.css" rel="stylesheet">
+<section class="hero is-primary">
+    <div class="hero-body">
+        <div class="container">
+            <h1 class="title">
+                Bien vivre chez soi
+            </h1>
+            <!-- Main container -->
+            <nav class="level">
+
+<!--Left side-->
+<figure class="media-left">
+    <p class="image is-64x64">
+      <img src="../asset/image/logo.png">
+    </p>
+  </figure>
+
+                <!-- Right side -->
+                <div class="level-right">
+                    <p class="level-item">Liens utiles</p>
+                    <p class="level-item"><a>Published</a></p>
+                    <p class="level-item"><a>Drafts</a></p>
+                    <p class="level-item"><a>Deleted</a></p>
+                    <p class="level-item"><a class="button is-success">New</a></p>
+
+
+                    <div class="level-item">
+                        <div class="field has-addons">
+                            <p class="control">
+                                <input class="input" type="text" placeholder="Find a post">
+                            </p>
+                            <p class="control">
+                                <button class="button">
+                                    Search
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</section>
