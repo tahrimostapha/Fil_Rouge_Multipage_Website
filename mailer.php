4b86fc3ed6bb9ee45e3cd2c29075dbaa7e487ad9
Tahri Mostapha <tahri.mostapha.pro@gmail.com>
2018-08-10 11:03:59 +0200

    PHPMailer : correct connecting bug [try 4]

--- mailer.php
+++ mailer.php
@@ -24,9 +24,9 @@ $mail->Host = 'smtp.gmail.com';
 // $mail->Host = gethostbyname('smtp.gmail.com');
 // if your network does not support SMTP over IPv6
 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 465;
+$mail->Port = 587;
 //Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'ssl';
+$mail->SMTPSecure = 'tls';
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
